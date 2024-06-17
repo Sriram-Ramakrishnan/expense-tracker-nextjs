5aebd37cccfd060df8c0b6857d4d574f6b574f3d
Sriram Ramakrishnan <srirampro107@gmail.com>
2024-06-17 11:26:05 +0530

    fix: removed error throwing from delete function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -125,7 +125,6 @@ export async function createInvoice(prevState: State, formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
